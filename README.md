a2c2b49daf6d13787d1d7a93d01b3a19175a5e6a
Samir Djelal <samir.djelal.webdesign@gmail.com>
2022-04-07 20:42:17 +0100

    ✨ Feature: CaptchaBuilder.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # captcha-rs
 
 [![Captcha-rs crate](https://img.shields.io/crates/v/captcha-rs.svg)](https://crates.io/crates/captcha-rs)
-[![CI](https://github.com/samirdjelal/captcha-rs/actions/workflows/CI.yml/badge.svg)](https://github.com/samirdjelal/captcha-rs/actions/workflows/CI.yml)
+[![test](https://github.com/samirdjelal/captcha-rs/actions/workflows/test.yml/badge.svg)](https://github.com/samirdjelal/captcha-rs/actions/workflows/test.yml)
 [![issues](https://img.shields.io/github/issues/samirdjelal/captcha-rs?color=%23ffc107)](https://github.com/samirdjelal/captcha-rs/issues)
 [![Downloads](https://img.shields.io/crates/d/captcha-rs)](https://crates.io/crates/captcha-rs)
 [![MIT License](https://img.shields.io/crates/l/captcha-rs)](LICENSE)
@@ -12,9 +12,9 @@
 
 Example pictures are as follows:
 
-![img-light-1.png](images/img-light-1.png) | ![img-light-2.png](images/img-light-2.png) | ![img-light-3.png](images/img-light-3.png) 
+![img-light-1.png](images/img-light-1.png) | ![img-light-2.png](images/img-light-2.png) | ![img-light-3.png](images/img-light-3.png)
 --- | --- | ---
-![img-dark-1.png](images/img-dark-1.png) | ![img-dark-2.png](images/img-dark-2.png) | ![img-dark-3.png](images/img-dark-3.png) 
+![img-dark-1.png](images/img-dark-1.png) | ![img-dark-2.png](images/img-dark-2.png) | ![img-dark-3.png](images/img-dark-3.png)
 
 ## Example
 
@@ -22,7 +22,7 @@ Add the following dependency to the Cargo.toml file:
 
 ```toml
 [dependencies]
-captcha-rs = "0.1.9"
+captcha-rs = "0.2.0"
 ```
 
 And then get started in your `main.rs`:
@@ -31,17 +31,24 @@ And then get started in your `main.rs`:
 use captcha_rs::Captcha;
 
 fn main() {
-   
-   let dark_mode = true;
-   let text_length = 5;
-   let width = 130;
-   let height = 40;
-   
-   let captcha = Captcha::new(text_length, width, height, dark_mode);
-   
-   println!("text: {}", captcha.text);
-   println!("base_img: {}", captcha.base_img);
-   
+	let dark_mode = true;
+	let text_length = 5;
+	let width = 130;
+	let height = 40;
+	
+	// generate a captcha with the given parameters
+	let captcha = Captcha::new(text_length, width, height, dark_mode);
+	
+	// generate a captcha using builder pattern
+	let captcha = CaptchaBuilder::new()
+		.length(5)
+		.width(130)
+		.height(40)
+		.dark_mode(false)
+		.build();
+	
+	println!("text: {}", captcha.text);
+	println!("base_img: {}", captcha.base_img);
 }
 ```
 
@@ -64,6 +71,4 @@ This project is licensed under the [MIT license](LICENSE).
 
 ### Contribution
 
-Unless you explicitly state otherwise, any contribution intentionally submitted
-for inclusion in `captcha-rs` by you, shall be licensed as MIT, without any additional
-terms or conditions.
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in `captcha-rs` by you, shall be licensed as MIT, without any additional terms or conditions.
--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,23 @@
 //! Generate a verification image.
 //!
 //! ```rust
-//! use captcha_rs::Captcha;
+//! use captcha_rs::{Captcha, CaptchaBuilder};
 //!
 //! let dark_mode = true;
 //! let text_length = 5;
 //! let width = 130;
 //! let height = 40;
 //!
+//! // generate a captcha with the given parameters
 //! let captcha = Captcha::new(text_length, width, height, dark_mode);
+//!
+//! // generate a captcha using builder pattern
+//! let captcha = CaptchaBuilder::new()
+//! 	.length(text_length)
+//! 	.width(width)
+//! 	.height(height)
+//! 	.dark_mode(dark_mode)
+//! 	.build();
 //!
 //! println!("text: {}", captcha.text);
 //! println!("base_img: {}", captcha.base_img);
@@ -21,48 +30,115 @@ use crate::captcha::{get_captcha, get_captcha_img};
 mod captcha;
 
 pub struct Captcha {
-    pub text: String,
-    pub base_img: String,
-    pub dark_mode: bool,
+	pub text: String,
+	pub base_img: String,
+	pub dark_mode: bool,
 }
 
 impl Captcha {
-    pub fn new(num: usize, width: u32, height: u32, dark_mode: bool) -> Self {
-        // Generate an array of captcha characters
-        let res = get_captcha(num);
-        let text = res.join("");
-        // Generate an image based on the verification code character array and convert the image into a base 64 string
-        let base_img = get_captcha_img(res, width, height, dark_mode);
-        Captcha {
-            text,
-            base_img,
-            dark_mode,
-        }
-    }
+	pub fn new(length: usize, width: u32, height: u32, dark_mode: bool) -> Self {
+		// Generate an array of captcha characters
+		let res = get_captcha(length);
+		let text = res.join("");
+		// Generate an image based on the verification code character array and convert the image into a base 64 string
+		let base_img = get_captcha_img(res, width, height, dark_mode);
+		Captcha {
+			text,
+			base_img,
+			dark_mode,
+		}
+	}
+}
+
+pub struct CaptchaBuilder {
+	length: Option<usize>,
+	width: Option<u32>,
+	height: Option<u32>,
+	dark_mode: Option<bool>,
+}
+
+impl CaptchaBuilder {
+	pub fn new() -> Self {
+		CaptchaBuilder {
+			length: None,
+			width: None,
+			height: None,
+			dark_mode: None,
+		}
+	}
+	
+	pub fn length(mut self, length: usize) -> Self {
+		self.length = Some(length);
+		self
+	}
+	
+	pub fn width(mut self, width: u32) -> Self {
+		self.width = Some(width);
+		self
+	}
+	
+	pub fn height(mut self, height: u32) -> Self {
+		self.height = Some(height);
+		self
+	}
+	
+	pub fn dark_mode(mut self, dark_mode: bool) -> Self {
+		self.dark_mode = Some(dark_mode);
+		self
+	}
+	
+	pub fn build(self) -> Captcha {
+		let length = self.length.unwrap_or(5);
+		let width = self.width.unwrap_or(130);
+		let height = self.height.unwrap_or(40);
+		let dark_mode = self.dark_mode.unwrap_or(false);
+		Captcha::new(length, width, height, dark_mode)
+	}
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::Captcha;
-
-    #[test]
-    fn it_generates_a_captcha() {
-        let dark_mode = false;
-        let text_length = 5;
-        let width = 130;
-        let height = 40;
-
-        let start = std::time::Instant::now();
-        let captcha = Captcha::new(text_length, width, height, dark_mode);
-        let duration = start.elapsed();
-        println!("Time elapsed in generating captcha() is: {:?}", duration);
-
-        assert_eq!(captcha.text.len(), 5);
-
-        let start_with = captcha.base_img.starts_with("data:image/png;base64,");
-        assert_eq!(start_with, true);
-
-        println!("text: {}", captcha.text);
-        println!("base_img: {}", captcha.base_img);
-    }
+	use crate::{Captcha, CaptchaBuilder};
+	
+	#[test]
+	fn it_generates_a_captcha() {
+		let dark_mode = false;
+		let text_length = 5;
+		let width = 130;
+		let height = 40;
+		
+		let start = std::time::Instant::now();
+		
+		let captcha = Captcha::new(text_length, width, height, dark_mode);
+		
+		let duration = start.elapsed();
+		println!("Time elapsed in generating captcha() is: {:?}", duration);
+		
+		assert_eq!(captcha.text.len(), 5);
+		
+		let start_with = captcha.base_img.starts_with("data:image/png;base64,");
+		assert_eq!(start_with, true);
+		
+		println!("text: {}", captcha.text);
+		println!("base_img: {}", captcha.base_img);
+	}
+	
+	#[test]
+	fn it_generate_captcha_using_builder(){
+		let start = std::time::Instant::now();
+		let captcha = CaptchaBuilder::new()
+			.length(5)
+			.width(130)
+			.height(40)
+			.dark_mode(false)
+			.build();
+		
+		let duration = start.elapsed();
+		println!("Time elapsed in generating captcha() is: {:?}", duration);
+		
+		assert_eq!(captcha.text.len(), 5);
+		
+		let start_with = captcha.base_img.starts_with("data:image/png;base64,");
+		assert_eq!(start_with, true);
+	}
 }
