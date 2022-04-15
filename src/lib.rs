9d0b8fc99f0f0ce55935b994cb833f662b6087f5
Samir Djelal <samir.djelal.webdesign@gmail.com>
2022-04-15 13:47:42 +0100

    font scale

--- src/lib.rs
+++ src/lib.rs
@@ -117,7 +117,7 @@ impl CaptchaBuilder {
 		draw_interference_ellipse(2, &mut image, dark_mode);
 		
 		gaussian_noise_mut(&mut image, (complexity.clone() - 1) as f64, ((10 * complexity.clone()) - 10) as f64, ((5 * complexity.clone()) - 5) as u64);
-		salt_and_pepper_noise_mut(&mut image, ((0.01 * complexity.clone() as f64) - 0.01) as f64, 1 * complexity.clone() as u64);
+		salt_and_pepper_noise_mut(&mut image, ((0.001 * complexity.clone() as f64) - 0.001) as f64, (0.5 * complexity.clone() as f64) as u64);
 		
 		
 		// Convert to base 64 string
@@ -166,7 +166,7 @@ mod tests {
 			.width(200)
 			.height(70)
 			.dark_mode(false)
-			.complexity(1)
+			.complexity(5)
 			.build();
 		
 		let duration = start.elapsed();
